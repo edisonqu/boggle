175b1ce15319f1ab447e09a6808a24d216b97582
edison <edisonquu@gmail.com>
2022-06-13 23:26:10 -0400

    Added a load of Methods, I just need to do the DFS part now

--- boggle.java
+++ boggle.java
@@ -1,11 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class boggle {
     static Random random = new Random();
     static ArrayList<String> wordList = new ArrayList<>();
+
     public static char[][][] fillDiceBoard(char[][][] diceBoard, String[][] diceArray) {
         for (int rowNum = 0; rowNum < 5; rowNum++) {
             for (int colNum = 0; colNum < 5; colNum++) {
@@ -18,7 +19,6 @@ public class boggle {
 
         return diceBoard;
     }
-
     // this is also used interchangeably with shake the board
     public static char[][] newBoard(char[][][] diceBoard){
         char[][] boggleBoard = new char[5][5];
@@ -31,27 +31,45 @@ public class boggle {
 
         return boggleBoard;
     }
-
     public static boolean isPassable(char[][] boggleBoard, int rowNum, int colNum){
-
-        if(rowNum >= boggleBoard[0].length || rowNum == -1 || colNum == -1 || colNum >= boggleBoard[0].length || ){
-            return true;
+         if(rowNum >= boggleBoard[0].length || rowNum == -1 || colNum == -1 || colNum >= boggleBoard[0].length){
+            return false;
         }
-
-        return false;
+        return true;
     }
-    public static int DFS(){
-        char[][] neighbors = new char[1][1];
-
+    public static ArrayList<Character> findNeighbors(char[][] boggleBoard, int rowNum, int colNum){
+        ArrayList<Character> neighbors = new ArrayList<Character>();
+        for(int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                if (isPassable(boggleBoard, rowNum + i, colNum + j)) {
+                    neighbors.add(boggleBoard[rowNum + i][rowNum + j]);
+                }
+            }
+        }
+        System.out.println(boggleBoard[rowNum][colNum]);
+        return neighbors;
+    }
+    public static HashSet<String> dictionaryWords() throws FileNotFoundException {
+        HashSet<String> dictionaryList = new HashSet<>();
+        File file = new File("dictionary.txt");
+        Scanner reader = new Scanner(file);
+        while (reader.hasNextLine()) {
+            String words = reader.nextLine();
+            dictionaryList.add(words);
+        }
+        reader.close();
+        return dictionaryList;
+    }
+    public static int DFS(char[][] boggleBoard, int rowNum, int colNum) {
+        ArrayList<Character> neighbors = findNeighbors(boggleBoard,rowNum,colNum);
+        System.out.println(neighbors);
 
         return -1;
     }
-
-
-
-
     public static ArrayList<String>  findWords(char[][] boggleBoard){
-
 
         return wordList;
     }
@@ -60,15 +78,13 @@ public class boggle {
         System.out.println(Arrays.deepToString(boggleBoard).replace("], ", "]\n"));
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
         String[][] diceArray = {{"AAAFRS", "AAEEEE", "AAFIRS" , "ADENNN", "AEEEEM"},{"AEEGMU" , "AEGMNN" , "AFIRSY" , "BJKQXZ" , "CCNSTW"} , {"CEIILT" , "CEILPT" , "CEIPST" , "DDLNOR" , "DHHLOR"} , {"DHHNOT" , "DHLNOR" , "EIIITT" , "EMOTTT" , "ENSSSU" }, {"FIPRSY" , "GORRVW" , "HIPRRY" , "NOOTUW" , "OOOTTU"}};
         char[][][] diceBoard = new char[5][5][6];
-
         fillDiceBoard(diceBoard, diceArray);
         char[][] boggleBoard = newBoard(diceBoard);
         printBoard(boggleBoard);
-        System.out.println("hii");
-
+        DFS(boggleBoard, 0,0);
 
     }
 }
